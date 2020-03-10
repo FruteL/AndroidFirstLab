7a414397462856454a1901dc97d7ca34bc4d4f07
FruteL <50375650+FruteL@users.noreply.github.com>
2020-03-10 16:01:18 +0200

    коменты

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,16 +7,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Поля ввода данных
     private EditText first;
     private EditText second;
+    //Кнопки
     private Button multi;
     private Button del;
     private Button plus;
     private Button minus;
+    //Вывод результата
     private TextView result;
+    //Переменные
     private double A;
     private double B;
 
@@ -25,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //связывание элементов интерфейса
         first = (EditText)findViewById(R.id.editTextFirts);
         second = (EditText)findViewById(R.id.editTextSecond);
         result = (TextView)findViewById(R.id.getResult);
 
+        //Слушатель на кнопки и выполнение положенных действий
         findViewById(R.id.buttonMulti).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,11 +69,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         }
-    public void getAB(){
-        if(!first.getText().toString().isEmpty()||!second.getText().toString().isEmpty()){
-            A = Double.parseDouble(first.getText().toString());
-            B = Double.parseDouble(second.getText().toString());
+    public void getAB() {   //функция отвечающая за получние значений А и В
+        if (!first.getText().toString().isEmpty() || !second.getText().toString().isEmpty()) { // проверка на пустоту
+            try {
+                A = Double.parseDouble(first.getText().toString()); //Преобразование строки в число
+                B = Double.parseDouble(second.getText().toString());
+            } catch (NumberFormatException e){
+                Toast.makeText(MainActivity.this, "Не используйте буквы", Toast.LENGTH_SHORT).show(); //соощение в случае если преобразование не возможно
+            }
+            }
         }
     }
-    }
+
 
